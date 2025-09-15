9983a8516a21146f0d2a081c7b2f710813a9da36
Fironn <shihomihashi@gmail.com>
2025-09-15 12:43:16 +0900

    heroサイズ修正・画像PC用

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { useState, useEffect } from 'react'
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
@@ -6,12 +7,22 @@ import Section5 from './Section5'
 
 export default function Hero() {
   const { t } = useTranslation()
+  const [isDesktop, setIsDesktop] = useState(false)
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsDesktop(window.innerWidth >= 768)
+    }
+    checkScreen()
+    window.addEventListener('resize', checkScreen)
+    return () => window.removeEventListener('resize', checkScreen)
+  }, [])
 
   return (
     <>
       <section className="hero">
         <img 
-          src="/img/1.png" 
+          src={isDesktop ? '/img/1_pc.jpg' : '/img/1.png'} 
           alt={t('hero.altText')}
         />
       </section>
