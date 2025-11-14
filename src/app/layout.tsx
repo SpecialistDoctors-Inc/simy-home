65ad28a47e4ef4b6c2fffaf603ccff7839d0abeb
Fironn <shihomihashi@gmail.com>
2025-11-14 18:52:26 +0900

    [feat]font add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
